1a9c720de08111eba0676480058b929d013c3042
umeanz <umean201@gmail.com>
2022-05-23 20:51:04 -0500

    add object && crash

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Making object is easier to handle charactor property.
 clearRect(): remove former draw
 
 arrow function
-```javascript
+```js
 cactusArray.forEach(a)=>{ //forEach: repeat function 
     a.draw();
 })
 ```
+
+array 
+```js
+arrayNamae= [];
+```
+
+time pass = frame pass
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight -100;
 
+var img2 = new Image();
+img2.src = 'dino.png'
+
 var dino = {
     x: 10,
     y: 200,
@@ -12,10 +15,12 @@ var dino = {
     draw(){
         ctx.fillStyle ='green';
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img2, this.x, this.y)
     }
 }
 
-dino.draw()
+var img1 = new Image();
+img1.src = 'cactus.png'
 
 class Cactus{
     constructor(){
@@ -27,28 +32,72 @@ class Cactus{
     draw(){
         ctx.fillStyle = 'red';
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img1, this.x, this.y)
     }
 
 }
 
-var timer = 0 // 
+var timer = 0; // 
 var cactusArray = [];
+var jumpTimer = 0;
+var animation;
 
 function eachFrameWroks(){
-    requestAnimationFrame(eachFrameWroks);
+    animation = requestAnimationFrame(eachFrameWroks);
     timer++;
     ctx.clearRect(0, 0, canvas.width, canvas.height); //잔상지우기
     
-    if(timer % 120 == 0){ //creat cactus every 120 and push in array 
-    var cactus = new Cactus();
-    cactusArray.push(cactus);
-    }
-    cactusArray.forEach((a)=>{
-        a.x--;
-    a.draw(); //draw all object from array
-    })
+    if (timer % 120 === 0){ //creat cactus every 60 and push in array 
+     var cactus = new Cactus();
+      cactusArray.push(cactus);
+     }
 
+    cactusArray.forEach((a, i, o) => {
+        if(a.x < 0){
+             o.splice(i, 1) //remove element from array
+        }
+         a.x--;
+         crashEachOther(dino, a);// in the loop keep cheking the crash
+
+         a.draw();//draw all object from array
+     })
+
+
+    //jump function
+    if(jump == true){
+        dino.y --; 
+        jumpTimer++;
+    }
+    if (jump == false){
+        if(dino.y < 200)
+        dino.y++;
+        }
+    if(jumpTimer > 100){
+        jump = false;
+        jumpTimer = 0
+    }
+       
     dino.draw();
 }
 
 eachFrameWroks();
+
+//collision check
+function crashEachOther(dino, cactus){
+  var xdiff = cactus.x - (dino.x+dino.width)
+  var ydiff = cactus.y - (dino.y+dino.height)
+  if(xdiff < 0 && ydiff <0){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cancelAnimationFrame(animation)
+  }
+
+}
+
+
+
+var jump = false;
+document.addEventListener('keydown', function(e){
+    if(e.code === 'Space'){
+        jump = true;
+    }
+})
